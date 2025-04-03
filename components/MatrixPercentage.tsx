95ae6a6e9a7b80f4345951f0a4521e5cf96f7fa4
MeesoPost <100311542+MeesoPost@users.noreply.github.com>
2025-04-03 14:48:52 +0200

    Add MatrixPercentage component styles and update import to CSS module

--- components/MatrixPercentage.tsx
+++ components/MatrixPercentage.tsx
@@ -1,11 +1,11 @@
-import "./MatrixPercentage.css";
+import styles from "./MatrixPercentage.module.css";
 
 const MatrixPercentage = () => {
   return (
-    <div className="matrix-percentage">
-      <div className="percentage-container">
-        <div className="title">HAK S3.5</div>
-        <div className="percentage">50%</div>
+    <div className={styles["matrix-percentage"]}>
+      <div className={styles["percentage-container"]}>
+        <div className={styles.title}>HAK S3.5</div>
+        <div className={styles.percentage}>50%</div>
       </div>
     </div>
   );
